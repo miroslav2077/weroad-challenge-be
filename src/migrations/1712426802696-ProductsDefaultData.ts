13a62b0405fb80166d83a198e5559e6b49eece2a
Mirko Grasso <mirko.grasso@gmbfinance.eu>
2024-04-07 11:58:22 +0200

    fixing migration order

--- src/migrations/1712426802696-ProductsDefaultData.ts
+++ src/migrations/1712426802696-ProductsDefaultData.ts
@@ -2,7 +2,7 @@ import { Product } from '../product/entities/product.entity';
 import { MigrationInterface, QueryRunner } from 'typeorm';
 import { products } from '../entities.seed';
 
-export class ProductsDefaultData1712063088562 implements MigrationInterface {
+export class ProductsDefaultData1712426802696 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     for (const product of products) {
       await queryRunner.manager.insert(Product, product);
